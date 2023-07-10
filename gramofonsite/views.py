1fac36ffae52a3ed60be678c2aa4622119ddc2d3
anetapec <aneta.pecka@gmail.com>
2023-07-10 23:26:29 +0200

    wyświetla stronę głowna

--- gramofonsite/views.py
+++ gramofonsite/views.py
@@ -3,5 +3,5 @@ from .models import Local
 
 def gramofonsite_list(request):
     locals = Local.objects.all()  # inquiry to database
-    return render(request, 'gramofonsite_list.html', {'locals': locals})
+    return render(request, 'gramofon/gramofonsite_list.html', {'locals': locals})
  
--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('gramofon', include('gramofonsite.urls'))
+    path('', include('gramofonsite.urls'))
 ]
