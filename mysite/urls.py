e3fb613022ffc93ae9ee3d3914f20041eb50a2cb
anetapec <aneta.pecka@gmail.com>
2023-07-13 00:35:33 +0200

    próba dodania zdj do strony głównej

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from gramofonsite import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('', include('gramofonsite.urls'))
+    path('', include('gramofonsite.urls')),
+    path('', views.gramofonsite_list, name='gramofonsite_list')
 ]
