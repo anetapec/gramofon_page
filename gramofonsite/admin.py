bc3356ba4007035a97c74724bbf7ff7f4d534d39
anetapec <aneta.pecka@gmail.com>
2023-07-13 00:01:00 +0200

    add image

--- gramofonsite/admin.py
+++ gramofonsite/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from .models import Local
 
 admin.site.register(Local)
+
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ['Title']
--- gramofonsite/models.py
+++ gramofonsite/models.py
@@ -7,7 +7,7 @@ class Local(models.Model):
     area = models.PositiveSmallIntegerField(default=200)
     nunber_of_people = models.PositiveSmallIntegerField(default=60)
     more_info = models.TextField(null=True, blank=True, default="")
-    # image = models.ImageField(upload_to="image", null=True, blank=True)
+    image = models.ImageField(upload_to="image", null=True, blank=True)
     # # title_page = models.CharField(max_length=20, null=True)
     # # street = models.CharField(max_length=20, null=True)
     # describe = models.OneToOneField(ShortDescribe, on_delete=models.CASCADE, null=True, blank=True)
@@ -16,7 +16,7 @@ class Local(models.Model):
         
         if self.number_of_levels is not None:
             return f" Lokal {self.number_of_levels} poziomowy o powierzchni {self.area} m2 do {self.nunber_of_people} osób." 
-        return f"Lokal o powierzchni {self.area} m2 do {self.nunber_of_people} osób." 
+        return f"Lokal o powierzchni {self.area} m2 do {self.nunber_of_people} osób. {self.image}" 
     
 
     # sprawdzenie git
