b688cf0a280475aee7099d67fe584c37bcb19b86
anetapec <aneta.pecka@gmail.com>
2023-07-14 23:19:01 +0200

    dodanie pierwszych bloków przełączanie miedzy str morska

--- gramofonsite/views.py
+++ gramofonsite/views.py
@@ -3,9 +3,9 @@ from .models import Local
 
 def gramofonsite_list(request):
     locals = Local.objects.all()  # inquiry to database
-    return render(request, 'gramofon/gramofonsite_list.html', {'locals': locals})
+    return render(request, '/home/aneta/software/repos/gramofon/gramofon_page/gramofonsite/templates/site/gramofon/gramofonsite_list.html', {'locals': locals})
 
 def morska(request):
     #locals = Local.objects.all()  # inquiry to database
-    return render(request, '/home/aneta/software/repos/gramofon/gramofon_page/gramofonsite/templates/gramofon/morska.html')
+    return render(request, '/home/aneta/software/repos/gramofon/gramofon_page/gramofonsite/templates/site/gramofon/morska.html')
   
