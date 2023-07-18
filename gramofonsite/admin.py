39815d13619e8e64b27f3ccbf263ed99f8070855
anetapec <aneta.pecka@gmail.com>
2023-07-19 00:08:04 +0200

    tworzenie wszystkiego w jednym modelu

--- gramofonsite/admin.py
+++ gramofonsite/admin.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
-from .models import Local, ShortDescribe, Room
+from .models import Local
 
 
 @admin.register(Local)
 class TopicAdmin(admin.ModelAdmin):
-    list_display = ['title' ]
+   list_display = ['title_local' ]
 
-admin.site.register(Room)
-admin.site.register(ShortDescribe)
-
+#admin.site.register(Local)
+# admin.site.register(ShortDescribe)
 
 
 
--- gramofonsite/models.py
+++ gramofonsite/models.py
@@ -1,44 +1,25 @@
 from django.db import models
 
-class ShortDescribe(models.Model):
-    title_page = models.CharField(max_length=20, null=True)
-    street = models.CharField(max_length=20, null=True)
-    # area = Local.area
-    # rooms =  models.Choices
-    # def __str__(self):
-        # return f"{self.title_page} \t {self.street}"
-
-    def __str__(self):
-        return self.title_page
-
-
-
-
-
-
-
-
-
-
-
-
-
-#    name = models.CharField(max_length=20)
-#    photo = models.ImageField(upload_to="image", null=True, blank=True)
-#    discrabe = models.TextField(null=True, blank=True, default="")
-#    add = models.ManyToManyField(Local)
        
 class Local(models.Model):
-    title = models.CharField(max_length=20)
+    logo_image = models.ImageField(upload_to="image", null=True, blank=True)
+    title_local = models.CharField(max_length=20)
+    title_page = models.CharField(max_length=20, null=True)
+    street = models.CharField(max_length=20, null=True)
     number_of_levels = models.PositiveIntegerField(blank=True, null=True,  default=0)
     area = models.PositiveSmallIntegerField(default=200)
     nunber_of_people = models.PositiveSmallIntegerField(default=60)
-    more_info = models.TextField(null=True, blank=True, default="")
-    image = models.ImageField(upload_to="image", null=True, blank=True)
+    more_info = models.CharField(max_length=20, null=True)
+    image_base = models.ImageField(upload_to="image", null=True, blank=True)
     title_page = models.CharField(max_length=20, null=True)
     street = models.CharField(max_length=20, null=True)
-    note = models.ForeignKey(ShortDescribe, on_delete=models.CASCADE, null=True, blank=True)
-
+    dance_room_title = models.CharField(max_length=20, null=True)
+    note_dance_room = models.TextField(max_length=500, null=True)
+    img_dance_room= models.ImageField(upload_to="image", null=True, blank=True)
+    note_toilet = models.TextField(max_length=500, null=True)
+    img_toilet= models.ImageField(upload_to="image", null=True, blank=True)
+    note_kitchen = models.TextField(max_length=500, null=True)
+    img_ktchen= models.ImageField(upload_to="image", null=True, blank=True)
     def __str__(self):
         
         if self.number_of_levels is not None:
@@ -48,34 +29,4 @@ class Local(models.Model):
     # def __str_(self):
         # return self.title
     
-
-class Room(models.Model):
-    DANCE_ROOM = "Dance"
-    TOILET = "WC"
-    KITCHEN = "Cook"
-
-    ROOMS = [
-        (DANCE_ROOM, "Sala Taneczna"),
-        (TOILET, "Toaleta"),
-        (KITCHEN, "Kuchnia",)
-    ]
-    description = models.CharField(max_length=500, choices=ROOMS, default=DANCE_ROOM)
-    image = models.ImageField(upload_to="image", null=True, blank=True)
-    note = models.ForeignKey(Local, on_delete=models.CASCADE, null=True, blank=True) 
-
-    def __str__(self): 
-        return self.description
-
-
-# class Morska(models.Model):
-
-
-    
-
-
-
-
-
-
-
 
