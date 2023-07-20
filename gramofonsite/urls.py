fa4b302999ae69d23f147e6feccac295fce06430
anetapec <aneta.pecka@gmail.com>
2023-07-20 22:30:06 +0200

    zmiana adresu url na morska

--- gramofonsite/urls.py
+++ gramofonsite/urls.py
@@ -8,7 +8,7 @@ from django.conf.urls.static import static
 app_name = 'gramofonsite'
 urlpatterns = [
     path('', views.gramofonsite_list, name='gramofonsite_list'), 
-    path('gramofon-morska', views.morska, name='gramofon-morska'),
+    path('morska', views.morska, name='morska'),
 
 
 
