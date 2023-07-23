c7455e7d3c8596a8a24b7090d72dd403ebadd4e7
anetapec <aneta.pecka@gmail.com>
2023-07-23 23:09:31 +0200

    próba załadowania zdjęcia jako tło

--- gramofonsite/views.py
+++ gramofonsite/views.py
@@ -3,11 +3,11 @@ from .models import Local
 
 def gramofonsite_list(request):
     locals = Local.objects.all()  # inquiry to database
-    return render(request, '/home/aneta/software/repos/gramofon/gramofonsite/templates/site/gramofon/gramofonsite_list.html', {'locals': locals})
+    return render(request, '/home/aneta/software/repos/gramofon/gramofon_page/gramofonsite/templates/site/gramofon/gramofonsite_list.html', {'locals': locals})
 
 def morska(request):
     morska_obj = Local.objects.get(id=2)  # inquiry to database
     context = {'locals': locals, 'morska_obj': morska_obj }
-    return render(request, '/home/aneta/software/repos/gramofon/gramofonsite/templates/site/gramofon/morska.html', context)
+    return render(request, '/home/aneta/software/repos/gramofon/gramofon_page/gramofonsite/templates/site/gramofon/morska.html', context)
 
   
